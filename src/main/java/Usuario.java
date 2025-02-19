08498812af697803416a895fd1d90b3b76a55354
Sharif Giraldo Obando <sharifgiraldo23@gmail.com>
2025-02-19 10:38:41 -0500

    Clase usuario

---
+++ src/main/java/Usuario.java
@@ -0,0 +1,63 @@
+public class Usuario {
+
+    private String nombre;
+    private String direccion;
+    private String cedula;
+    private String correo;
+    private String contrasenia;
+
+    public Usuario(String nombre, String direccion, String cedula, String correo, String contrasenia) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.cedula = cedula;
+        this.correo = correo;
+        this.contrasenia = contrasenia;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public void recargarBilletera(float ingreso) {
+
+    }
+}
+
+
+
