cbbffc4cad48cd3eed0ac5d8c8cd84ab7071bc8d
JuanHG07 <juan.hernandezg@uqvirtual.edu.co>
2025-02-19 10:38:17 -0500

    Banco

--- src/main/java/Banco.java
+++ src/main/java/Banco.java
@@ -1,4 +1,52 @@
+import java.util.LinkedList;
+
 public class Banco {
-    private String nombre;
-    private String direccion;
+    public String nombre;
+    public LinkedList<Usuario> usuarios;
+    public LinkedList<Transaccion> transacciones;
+    public LinkedList<BilleteraVirtual> billeterasVirtuales;
+
+    public Banco(String nombre, LinkedList<Usuario> usuarios, LinkedList<Transaccion> transacciones, LinkedList<BilleteraVirtual> billeterasVirtuales) {
+        this.nombre = nombre;
+        this.usuarios = usuarios;
+        this.transacciones = transacciones;
+        this.billeterasVirtuales = billeterasVirtuales;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LinkedList<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(LinkedList<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+
+    public LinkedList<Transaccion> getTransacciones() {
+        return transacciones;
+    }
+
+    public void setTransacciones(LinkedList<Transaccion> transacciones) {
+        this.transacciones = transacciones;
+    }
+
+    public LinkedList<BilleteraVirtual> getBilleterasVirtuales() {
+        return billeterasVirtuales;
+    }
+
+    public void setBilleterasVirtuales(LinkedList<BilleteraVirtual> billeterasVirtuales) {
+        this.billeterasVirtuales = billeterasVirtuales;
+    }
+
+    @Override
+    public String toString() {
+        return "Banco{" + "nombre='" + nombre + '\'' + ", usuarios=" + usuarios + ", transacciones=" + transacciones + ", billeterasVirtuales=" + billeterasVirtuales + '}';
+    }
 }
