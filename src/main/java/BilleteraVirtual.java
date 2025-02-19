3816d93b469b9ec836654d9babdb72b0c4c878ba
SntOp7 <santiago.ospinas@uqvirtual.edu.co>
2025-02-19 10:36:49 -0500

    billetera

---
+++ src/main/java/BilleteraVirtual.java
@@ -0,0 +1,44 @@
+public class BilleteraVirtual {
+    private int numeroAleatorio;
+    private float saldo;
+    private Usuario usuario;
+
+    public BilleteraVirtual(int numeroAleatorio, float saldo, Usuario usuario) {
+        this.numeroAleatorio = numeroAleatorio;
+        this.saldo = saldo;
+        this.usuario = usuario;
+    }
+
+    public int getNumeroAleatorio() {
+        return numeroAleatorio;
+    }
+
+    public void setNumeroAleatorio(int numeroAleatorio) {
+        this.numeroAleatorio = numeroAleatorio;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    @Override
+    public String toString() {
+        return "BilleteraVirtual{" +
+                "numeroAleatorio=" + numeroAleatorio +
+                ", saldo=" + saldo +
+                ", usuario=" + usuario +
+                '}';
+    }
+}
