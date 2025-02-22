8d8d019c9325aeb70b1297f7b05ce1063787ff59
Sharif Giraldo Obando <sharifgiraldo23@gmail.com>
2025-02-21 20:15:47 -0500

    Métodos Faltantes BilleteraVirtual

--- src/main/java/BilleteraVirtual.java
+++ src/main/java/BilleteraVirtual.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 import java.util.LinkedList;
 
 public class BilleteraVirtual {
@@ -74,6 +75,57 @@ public class BilleteraVirtual {
         } else {
             billetera.setSaldo(billetera.getSaldo() + ingreso);
         }
+    }
+
+
+    public LinkedList<Transaccion> consultarTransaccionesPeriodo(LocalDate periodo) {
+        LinkedList<Transaccion> transaccionesFiltradasPeriodo = new LinkedList<>();
+
+        for (Transaccion t : transacciones) {
+            if (t.getFecha().toLocalDate().equals(periodo)) {
+                transaccionesFiltradasPeriodo.add(t);
+            }
+        }
+
+        return transaccionesFiltradasPeriodo;
+    }
+
+
+    public LinkedList<Transaccion> calcularPeriodo(LocalDate fecha1, LocalDate fecha2) {
+        LinkedList<Transaccion> transaccionesCalcularPerioro = new LinkedList<>();
+
+        for (Transaccion t : transacciones) {
+            LocalDate fechaTransaccion = t.getFecha().toLocalDate();
+            if ((fechaTransaccion.isAfter(fecha1) || fechaTransaccion.equals(fecha1)) &&
+                    (fechaTransaccion.isBefore(fecha2) || fechaTransaccion.equals(fecha2))) {
+                transaccionesCalcularPerioro.add(t);
+            }
+        }
+
+        return transaccionesCalcularPerioro;
+    }
+
+
+
+    public String calcularPorcentajeGastosIngresos(LocalDate periodo) {
+        float ingresos = 0;
+        float gastos = 0;
+
+        for (Transaccion t : transacciones) {
+            if (t.getFecha().toLocalDate().equals(periodo)) {
+                if (t.getDestino().equals(this)) {
+                    ingresos += t.getMonto();
+                } else if (t.getOrigen().equals(this)) { 
+                    gastos += t.getMonto();
+                }
+            }
+        }
+
+        float total = ingresos + gastos;
+        float porcentajeIngresos = (total > 0) ? (ingresos / total) * 100 : 0;
+        float porcentajeGastos = (total > 0) ? (gastos / total) * 100 : 0;
+
+        return "Ingresos: " + porcentajeIngresos + "%, Gastos: " + porcentajeGastos + "%";
     }
 
 
